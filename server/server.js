8752e7cdf900af244089ac726aa041564a6a68f4
hamdan998 <hamdaab@ac.sce.ac.il>
2026-05-30 10:00:00 +0300

    BSPMT12-327 feat: guide receives email reminder before tour
    - Start reminderJob cron after MongoDB connects in server.js
    - Cron also sends guide reminder email (tour title, date, time, booked slots)
    - Reminder fires hourly for tours starting in next 24-25h window

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./app');
 
+const { startReminderJob } = require('./jobs/reminderJob');
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    startReminderJob();
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err);
